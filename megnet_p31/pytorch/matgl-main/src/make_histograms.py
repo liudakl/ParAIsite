b4b56fd3820fb433d72f7655f50737d525b8c4b4
Liudmyla Klochko <klochko.lyuda@gmail.com>
2024-09-13 12:57:23 +0200

    final

--- megnet_p31/pytorch/matgl-main/src/make_histograms.py
+++ megnet_p31/pytorch/matgl-main/src/make_histograms.py
@@ -8,11 +8,11 @@ Created on Tue Aug 20 14:23:11 2024
 
 import matplotlib.pyplot as plt
 from cmcrameri import cm
-import numpy as np
 import seaborn as sns
 import pandas as pd
 # Data from the table
 
+'''
 # Data from the table
 data = {
     'L96_1': [0.54, 2.53, 1.74, 2.16],
@@ -31,18 +31,17 @@ data = {
 df = pd.DataFrame(data, index=['L96', 'HH143', 'MIX', 'AFLOW'])
 
 # Create the heatmap
-plt.figure(figsize=(10, 8), dpi=700)
+plt.figure(figsize=(12, 10), dpi=700)
 ax = sns.heatmap(df.T, annot=True, cmap=cm.vik, fmt='.2f', linewidths=3,cbar_kws={'shrink': 1})
 ax.collections[0].colorbar.set_label("MAPE",fontsize=20)
 plt.ylabel('Train on', fontsize=20)
-plt.xlabel('Train on', fontsize=20)
+plt.xlabel('Test on', fontsize=20)
 
 plt.xticks(rotation=45, fontsize=15)
 plt.yticks(rotation=0, fontsize=15)
-plt.show()
 plt.savefig('/home/lklochko/Desktop/ProjPostDoc/GitHub/ParAIsite/megnet_p31/pytorch/matgl-main/src/heatmap_results.eps', format='eps')
+plt.show()
 
-cmap = cm.batlow  # Choose a color map, e.g., 'coolwarm'
 
 
 #colors = ['skyblue', 'lightsalmon', 'lightpink', 'lightblue', 'lightyellow', 'lightgrey', 'cyan']
@@ -59,6 +58,7 @@ df = df[['model', 'target', 'mape_mean','mape_std']]
 df['model'] = df['model'].replace('paper31', 'MEGNet')
 df['model'] = df['model'].replace('paper49', 'ALIGNN')
 df['model'] = df['model'].replace('paper60', 'CrabNet')
+cmap = cm.batlow  # Choose a color map, e.g., 'coolwarm'
 
 mask = df['mape_mean'] < 0.51
 
@@ -84,5 +84,66 @@ plt.yticks(ticks=range(len(df)), labels=df['model'],fontsize=15)
 plt.xticks(fontsize=15)
 
 plt.xlabel('MAPE',fontsize=15)
-plt.show()
 plt.savefig('/home/lklochko/Desktop/ProjPostDoc/GitHub/ParAIsite/megnet_p31/pytorch/matgl-main/src/mape_mlp_test.eps', format='eps')
+
+plt.show()
+
+'''
+
+
+
+dataset_name_TRAIN = 'L96'
+maxRuns = 9
+
+y_temp_1  = 0 
+y_temp_2  = 0 
+y_temp_3  = 0 
+
+m = 0 
+for nRuns in range (1,maxRuns+1):
+    df = pd.read_csv("logs/MEGNet_training_no_weights_%s_%s/version_0/metrics.csv"%(dataset_name_TRAIN,nRuns))
+    y_temp_1 = df["val_Total_Loss"].dropna().reset_index().drop(columns='index')  + y_temp_1
+    df = pd.read_csv("logs/MEGNet_m1_training_%s_%s/version_0/metrics.csv"%(dataset_name_TRAIN,nRuns))
+    y_temp_2 = df["val_Total_Loss"].dropna().reset_index().drop(columns='index')  + y_temp_2
+    df = pd.read_csv("logs/MEGNet_m1_best_model_double_training_%s_%s/version_0/metrics.csv"%(dataset_name_TRAIN,nRuns))
+    y_temp_3 = df["val_Total_Loss"].dropna().reset_index().drop(columns='index')  + y_temp_3
+    m = m + 1
+    
+y_v1 = y_temp_1 / m    
+y_v2 = y_temp_2 / m 
+y_v3 = y_temp_3 / m 
+
+x = range(len(y_v1))
+plt.figure(figsize=(14, 8),dpi=700)
+
+#plt.plot(x, y_v1,label='step 1')
+#plt.plot(x, y_v2,label='step 2')
+#plt.plot(x, y_v3,label='step 3')
+
+plt.scatter(x, y_v1,label='step 1',marker='o',s=80,color='tomato')
+plt.scatter(x, y_v2,label='step 2',marker='^',s=80,color='darkcyan')
+plt.scatter(x, y_v3,label='step 3',marker='+',s=300,color='olivedrab')
+
+plt.ylabel('Validation Loss', fontsize=20)
+plt.xlabel('Epochs', fontsize=20)
+
+
+plt.xticks(rotation=45, fontsize=15)
+plt.yticks(rotation=0, fontsize=15)
+
+plt.legend(fontsize=15)
+plt.title('%s database'%(dataset_name_TRAIN), fontsize=15)
+
+#plt.savefig('/home/lklochko/Desktop/ProjPostDoc/GitHub/ParAIsite/megnet_p31/pytorch/matgl-main/src/loss_all_steps_%s.eps'%(dataset_name_TRAIN), format='eps')
+
+plt.show()
+
+
+
+
+
+
+
+
+
+
